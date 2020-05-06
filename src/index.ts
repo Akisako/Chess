c9d6d1258eab54bde48cff2ad5210516ece0bbef
Akisako <46268842+Akisako@users.noreply.github.com>
2020-05-06 22:41:45 +0200

    centre le canvas

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-let h1 = document.createElement("h1")
-h1.textContent = document.title
-document.body.appendChild(h1)
-let canvas = document.createElement("canvas")
-document.body.appendChild(canvas)
+let h1 = document.createElement("h1");
+h1.textContent = document.title;
+document.body.appendChild(h1);
+let canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
