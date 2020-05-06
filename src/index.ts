738db05910f55c672c2241604df405a7f99dfccf
Akisako <46268842+Akisako@users.noreply.github.com>
2020-05-06 20:52:06 +0200

    Chess

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let h1 = document.createElement("h1")
+h1.textContent = document.title
+document.body.appendChild(h1)
+let canvas = document.createElement("canvas")
+document.body.appendChild(canvas)
